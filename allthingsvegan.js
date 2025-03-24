1874ca84c236c6f827e8aac0a9456e9ba3e98b9a
Neil McCaffrey <neil.mccaffrey83@gmail.com>
2025-03-24 15:24:35 -0400

    added categories and items

--- allthingsvegan.js
+++ allthingsvegan.js
@@ -1,21 +1,46 @@
 const allthingsvegan = [
     {
         id: 1,
-        category: "Apparel",
+        category: "Food",
         items: [
-          { name: "Project Animal Freedom", description: '', website: "https://www.projectanimalfreedom.org/" },
-          { name: "Matt & Nat", description: '', website: "https://us.mattandnat.com/" },
+          { name: 'Providence Vegan Deli', description: "Deli sliced seitan by the pound available for shipping.", website: 'https://www.providencevegandeli.com/'},
+          { name: 'Huel', description: "100% plant-based protein powder", website: 'https://huel.com/'}, 
+          { name: "Purple Carrot", description: 'Vegan meal kits delivered to your door.', website: "https://www.purplecarrot.com/" },
+          { name: "Raaka", description: 'Vegan chocolate', website: "https://www.raakachocolate.com/" },
+          { name: 'The Naughty Cookie', description: '100% vegan cookies shipped to your door.', website: 'https://www.thenaughtycookie.com/'},
         ],
     },
     {
         id: 2,
-        category: "Food",
+        category: "Apparel",
         items: [
-          { name: 'Providence Vegan Deli', description: "Deli sliced seitan by the pound available for shipping.", website: 'https://www.providencevegandeli.com/'},
-          { name: "Beyond Meat", website: "https://www.beyondmeat.com/" },
-          { name: "Impossible Foods", website: "https://impossiblefoods.com/" },
+          { name: 'Immaculate Vegan', description: 'Just about everything you could want. Clothes, shoes, beauty, home. ', website: 'https://immaculatevegan.com/'},
+          { name: "Matt & Nat", description: 'Vegan leather bags & designer accessories.', website: "https://us.mattandnat.com/" },
+          { name: 'Toad & Co', description: "Vegan friendly clothes made out of plant-based and man-made fibers.", website: 'https://www.toadandco.com/'},
         ],
-      },
+    },
+    {
+        id: 3,
+        category: "Animal Rights Organizations",
+        items: [
+          { name: "Project Animal Freedom", description: 'Features a store to support their mission.', website: "https://www.projectanimalfreedom.org/" },
+          { name: "PETA", description: 'Has a shop, and a comprehensive list of companies that do not test on animals.', website: 'https://www.peta.org/'}, 
+          { name: "ASPCA", description: 'Help shelters and pets, improves laws for animals, investigations and rescues.', website: "https://www.aspca.org/"},
+          { name: "American Humane Society", description: 'Sanctuary, disaster response, emergency training.', website: 'https://www.americanhumane.org/'}
+        ]
+    },
+    {
+        id: 4,
+        category: "Household & Cleaning Products",
+        items: [
+          { name: "Greenzla", description: 'Bamboo toothbrushes, reusable cotton pads, cutlery.', website: 'https://greenzla.com/'},
+          { name: "Bambooee", description: 'Pillows with charcol infused covers, reusable bamboo paper towels.', website: 'https://bambooee.com/'},
+          { name: 'Branch Basics', description: 'Plant and mineral based cleaning products.', website: 'https://branchbasics.com/'},
+          { name: 'Blueland', description: 'Plant and mineral based with reusable bottles refilled with plastic-free tablets.', website: 'https://www.blueland.com/'}
+        ]
+    },
+
+    
 ];
 
 export default allthingsvegan;
